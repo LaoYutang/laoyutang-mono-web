4756dc35d76719d404695c74ea1df40cbbb9d292
ewen.zhang <ewen.zhang@welab-inc.com>
2023-02-24 10:33:46 +0800

    feature portal增加unplugin-icons自动引入

--- packages/portal/vite.config.ts
+++ packages/portal/vite.config.ts
@@ -5,6 +5,8 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import * as path from 'path'
 import { UtilsImport } from 'imports'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
   server: {
@@ -37,7 +39,16 @@ export default defineConfig({
     }),
     Components({
       dts: 'src/components.d.ts',
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'icon', // 自动引入的Icon组件统一前缀，默认为 i，设置false为不需要前缀
+          // {prefix}-{collection}-{icon} 使用组件解析器时，您必须遵循名称转换才能正确推断图标。
+          // alias: { park: 'icon-park' } 集合的别名
+          // enabledCollections: ['ep'], // 这是可选的，默认启用 Iconify 支持的所有集合['mdi']
+        }),
+      ],
     }),
+    Icons({ autoInstall: true, compiler: 'vue3' }),
   ],
 })
